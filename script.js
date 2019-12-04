7e3b71dcbeee10fa59f58854f76d19bac4662c9e
Tabish1511 <khaqantabish@gmail.com>
2019-12-04 21:07:47 +0500

    reverting back

--- script.js
+++
@@ -1,8 +0,0 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-
-console.log(css);
-console.log(color1);
-console.log(color2);
